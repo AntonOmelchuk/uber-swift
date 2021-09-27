2b695c0c78a32c628d56bbaf3cd15a40853a4722
Anton Omelchuk <kino_kg@ukr.net>
2021-09-27 21:00:46 +0300

    delegate location input tap

--- Uber/View/LocationInputActivationView.swift
+++ Uber/View/LocationInputActivationView.swift
@@ -7,9 +7,15 @@
 
 import UIKit
 
+protocol LocationInputActivetaViewDelegate: class {
+    func presentLocationInputView()
+}
+
 class LocationInputActivationView: UIView {
     
     // MARK: - Properties
+    
+    weak var delegate: LocationInputActivetaViewDelegate?
     
     private let indicatorView: UIView = {
         let view = UIView()
@@ -45,9 +51,18 @@ class LocationInputActivationView: UIView {
         
         addSubview(placeholderLabel)
         placeholderLabel.centerY(inView: self, leftAnchor: indicatorView.rightAnchor, paddingLeft: 20)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(presentLocationInputView))
+        addGestureRecognizer(tap)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func presentLocationInputView() {
+        delegate?.presentLocationInputView()
     }
 }
