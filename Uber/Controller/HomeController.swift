fb5036ddb88c55cfc190ba35f177e4e5cfc6fc31
Anton Omelchuk <kino_kg@ukr.net>
2021-10-02 10:32:00 +0300

    update driver position

--- Uber/Controller/HomeController.swift
+++ Uber/Controller/HomeController.swift
@@ -38,6 +38,7 @@ class HomeController: UIViewController {
         enableLocationServices()
         fetchUserData()
         fetchDrivers()
+        signOut()
     }
     
     // MARK: - API
@@ -55,7 +56,20 @@ class HomeController: UIViewController {
             guard let coordinate = driver.location?.coordinate else { return }
             let annotation = DriverAnnotation(uid: driver.uid, coordinate: coordinate)
             
-            self.mapView.addAnnotation(annotation)
+            var driverIsVisible: Bool {
+                return self.mapView.annotations.contains { annotation in
+                    guard let driverAnnotation = annotation as? DriverAnnotation else { return false }
+                    if driverAnnotation.uid == driver.uid {
+                        driverAnnotation.updateAnnotationPosition(withCoordinate: coordinate)
+                        return true
+                    }
+                    return false
+                }
+            }
+            
+            if !driverIsVisible {
+                self.mapView.addAnnotation(annotation)
+            }
         }
     }
     
