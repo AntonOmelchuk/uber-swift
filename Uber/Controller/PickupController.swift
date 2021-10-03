84c0d13e5e21fe36ba2e6dde4ee29bd44402f1a0
Anton Omelchuk <kino_kg@ukr.net>
2021-10-03 16:42:06 +0300

    add marker to the PickupController mapView

--- Uber/Controller/PickupController.swift
+++ Uber/Controller/PickupController.swift
@@ -57,6 +57,7 @@ class PickupController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureMapView()
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -76,6 +77,16 @@ class PickupController: UIViewController {
     // MARK: - API
     
     // MARK: - Hepler Functions
+    
+    func configureMapView() {
+        let region = MKCoordinateRegion(center: trip.pickupCoordinates, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: false)
+        
+        let anno = MKPointAnnotation()
+        anno.coordinate = trip.pickupCoordinates
+        mapView.addAnnotation(anno)
+        mapView.selectAnnotation(anno, animated: true)
+    }
     
     func configureUI() {
         view.backgroundColor = .backgroundColor
